41cef46bb50f878bad42e7fe54adfbc7192bc57d
Nukepayload2 <1939357182@qq.com>
2017-01-23 01:12:29 +0800

    添加 VB 模板

---
+++ Templates/install.bat
@@ -0,0 +1,3 @@
+copy Assembly-VisualBasic.zip "%USERPROFILE%\Documents\Visual Studio 2017\Templates\ProjectTemplates\Visual Basic\Assembly-VisualBasic.zip"
+copy MonoBehavior-VisualBasic.zip "C:\Users\19393\Documents\Visual Studio 2017\Templates\ItemTemplates\Visual Basic\MonoBehavior-VisualBasic.zip"
+pause
