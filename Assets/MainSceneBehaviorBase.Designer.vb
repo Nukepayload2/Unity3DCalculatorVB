1121ba11e8598bf647523002ad114c0c3c9298c5
Nukepayload2 <1939357182@qq.com>
2017-01-22 23:52:54 +0800

    初始提交

---
+++ Assets/MainSceneBehaviorBase.Designer.vb
@@ -0,0 +1,211 @@
+﻿Imports UnityEngine
+
+Partial Public Class MainSceneBehaviorBase
+
+    Partial Private Sub Awake()
+    End Sub
+
+    Partial Private Sub FixedUpdate()
+    End Sub
+
+    Partial Private Sub LateUpdate()
+    End Sub
+
+    Partial Private Sub OnAnimatorIK(ByVal layerIndex As Integer)
+    End Sub
+
+    Partial Private Sub OnAnimatorMove()
+    End Sub
+
+    Partial Private Sub OnApplicationFocus(ByVal focus As Boolean)
+    End Sub
+
+    Partial Private Sub OnApplicationPause(ByVal pause As Boolean)
+    End Sub
+
+    Partial Private Sub OnApplicationQuit()
+    End Sub
+
+    Partial Private Sub OnAudioFilterRead(ByVal data As Single(), ByVal channels As Integer)
+    End Sub
+
+    Partial Private Sub OnBecameInvisible()
+    End Sub
+
+    Partial Private Sub OnBecameVisible()
+    End Sub
+
+    Partial Private Sub OnBeforeTransformParentChanged()
+    End Sub
+
+    Partial Private Sub OnCanvasGroupChanged()
+    End Sub
+
+    Partial Private Sub OnCollisionEnter(ByVal collision As UnityEngine.Collision)
+    End Sub
+
+    Partial Private Sub OnCollisionEnter2D(ByVal collision As Collision2D)
+    End Sub
+
+    Partial Private Sub OnCollisionExit(ByVal collision As UnityEngine.Collision)
+    End Sub
+
+    Partial Private Sub OnCollisionExit2D(ByVal collision As Collision2D)
+    End Sub
+
+    Partial Private Sub OnCollisionStay(ByVal collision As UnityEngine.Collision)
+    End Sub
+
+    Partial Private Sub OnCollisionStay2D(ByVal collision As Collision2D)
+    End Sub
+
+    Partial Private Sub OnConnectedToServer()
+    End Sub
+
+    Partial Private Sub OnControllerColliderHit(ByVal hit As ControllerColliderHit)
+    End Sub
+
+    Partial Private Sub OnDestroy()
+    End Sub
+
+    Partial Private Sub OnDisable()
+    End Sub
+
+    Partial Private Sub OnDisconnectedFromMasterServer(ByVal info As NetworkDisconnection)
+    End Sub
+
+    Partial Private Sub OnDisconnectedFromServer(ByVal info As NetworkDisconnection)
+    End Sub
+
+    Partial Private Sub OnDrawGizmos()
+    End Sub
+
+    Partial Private Sub OnDrawGizmosSelected()
+    End Sub
+
+    Partial Private Sub OnEnable()
+    End Sub
+
+    Partial Private Sub OnFailedToConnect(ByVal [error] As NetworkConnectionError)
+    End Sub
+
+    Partial Private Sub OnFailedToConnectToMasterServer(ByVal [error] As NetworkConnectionError)
+    End Sub
+
+    Partial Private Sub OnGUI()
+    End Sub
+
+    Partial Private Sub OnJointBreak(ByVal breakForce As Single)
+    End Sub
+
+    Partial Private Sub OnJointBreak2D(ByVal joint As Joint2D)
+    End Sub
+
+    Partial Private Sub OnLevelWasLoaded(ByVal level As Integer)
+    End Sub
+
+    Partial Private Sub OnMasterServerEvent(ByVal msEvent As MasterServerEvent)
+    End Sub
+
+    Partial Private Sub OnMouseDown()
+    End Sub
+
+    Partial Private Sub OnMouseDrag()
+    End Sub
+
+    Partial Private Sub OnMouseEnter()
+    End Sub
+
+    Partial Private Sub OnMouseExit()
+    End Sub
+
+    Partial Private Sub OnMouseOver()
+    End Sub
+
+    Partial Private Sub OnMouseUp()
+    End Sub
+
+    Partial Private Sub OnMouseUpAsButton()
+    End Sub
+
+    Partial Private Sub OnNetworkInstantiate(ByVal info As NetworkMessageInfo)
+    End Sub
+
+    Partial Private Sub OnParticleCollision(ByVal other As GameObject)
+    End Sub
+
+    Partial Private Sub OnParticleTrigger()
+    End Sub
+
+    Partial Private Sub OnPlayerConnected(ByVal player As NetworkPlayer)
+    End Sub
+
+    Partial Private Sub OnPlayerDisconnected(ByVal player As NetworkPlayer)
+    End Sub
+
+    Partial Private Sub OnPostRender()
+    End Sub
+
+    Partial Private Sub OnPreCull()
+    End Sub
+
+    Partial Private Sub OnPreRender()
+    End Sub
+
+    Partial Private Sub OnRectTransformDimensionsChange()
+    End Sub
+
+    Partial Private Sub OnRectTransformRemoved()
+    End Sub
+
+    Partial Private Sub OnRenderImage(ByVal source As RenderTexture, ByVal destination As RenderTexture)
+    End Sub
+
+    Partial Private Sub OnRenderObject()
+    End Sub
+
+    Partial Private Sub OnSerializeNetworkView(ByVal stream As BitStream, ByVal info As NetworkMessageInfo)
+    End Sub
+
+    Partial Private Sub OnServerInitialized()
+    End Sub
+
+    Partial Private Sub OnTransformChildrenChanged()
+    End Sub
+
+    Partial Private Sub OnTransformParentChanged()
+    End Sub
+
+    Partial Private Sub OnTriggerEnter(ByVal other As Collider)
+    End Sub
+
+    Partial Private Sub OnTriggerEnter2D(ByVal collision As Collider2D)
+    End Sub
+
+    Partial Private Sub OnTriggerExit(ByVal other As Collider)
+    End Sub
+
+    Partial Private Sub OnTriggerExit2D(ByVal collision As Collider2D)
+    End Sub
+
+    Partial Private Sub OnTriggerStay(ByVal other As Collider)
+    End Sub
+
+    Partial Private Sub OnTriggerStay2D(ByVal collision As Collider2D)
+    End Sub
+
+    Partial Private Sub OnValidate()
+    End Sub
+
+    Partial Private Sub OnWillRenderObject()
+    End Sub
+
+    Partial Private Sub Reset()
+    End Sub
+
+    Partial Private Sub Start()
+    End Sub
+
+    Partial Private Sub Update()
+    End Sub
+End Class
---
+++ Assets/MainSceneBehaviorBase.vb
@@ -0,0 +1,144 @@
+﻿Imports UnityEngine
+Imports UnityEngine.UI
+
+Public Class MainSceneBehaviorBase
+    Inherits MonoBehaviour
+
+    Dim calc As New AnalysisHelper.ExpressionCalculator
+    Dim justEvaled As Boolean
+    Dim justFaulted As Boolean
+
+    Public TblValue As Text
+
+    Private Sub Start()
+        Debug.Log($"提示：游戏对象 {gameObject.name} 是用 Visual Basic 2017 写的！")
+    End Sub
+
+    Private Sub AppendChar(ch As Char)
+        Dim txt = TblValue.text
+        If justFaulted Then
+            TblValue.text = String.Empty
+            justEvaled = True
+        End If
+        If justEvaled Then
+            If Char.IsDigit(ch) Then
+                TblValue.text = ch
+            ElseIf ch = "."c Then
+                TblValue.text = "0."
+            Else
+                TblValue.text += ch
+            End If
+        Else
+            If txt = "0" AndAlso ch <> "." Then
+                TblValue.text = ch
+            Else
+                TblValue.text += ch
+            End If
+        End If
+        justEvaled = False
+        justFaulted = False
+    End Sub
+
+    Public Sub BtnAdd_Click()
+        AppendChar("+"c)
+    End Sub
+
+    Public Sub BtnMinus_Click()
+        AppendChar("-"c)
+    End Sub
+
+    Public Sub BtnMultiply_Click()
+        AppendChar("*"c)
+    End Sub
+
+    Public Sub BtnDivide_Click()
+        AppendChar("/"c)
+    End Sub
+
+    Public Sub BtnDot_Click()
+        AppendChar("."c)
+    End Sub
+
+    Public Sub Btn0_Click()
+        AppendChar("0"c)
+    End Sub
+
+    Public Sub Btn1_Click()
+        AppendChar("1"c)
+    End Sub
+
+    Public Sub Btn2_Click()
+        AppendChar("2"c)
+    End Sub
+
+    Public Sub Btn3_Click()
+        AppendChar("3"c)
+    End Sub
+
+    Public Sub Btn4_Click()
+        AppendChar("4"c)
+    End Sub
+
+    Public Sub Btn5_Click()
+        AppendChar("5"c)
+    End Sub
+
+    Public Sub Btn6_Click()
+        AppendChar("6"c)
+    End Sub
+
+    Public Sub Btn7_Click()
+        AppendChar("7"c)
+    End Sub
+
+    Public Sub Btn8_Click()
+        AppendChar("8"c)
+    End Sub
+
+    Public Sub Btn9_Click()
+        AppendChar("9"c)
+    End Sub
+
+    Public Sub BtnCalculate_Click()
+        Try
+            TblValue.text = calc.Eval(TblValue.text).ToString("#.######")
+            justFaulted = False
+        Catch ex As Exception
+            TblValue.text = ex.Message
+            justFaulted = True
+        End Try
+        justEvaled = True
+    End Sub
+
+    Public Sub BtnRightDiv_Click()
+        AppendChar("\"c)
+    End Sub
+
+    Public Sub BtnLeftParenthesis_Click()
+        AppendChar("("c)
+    End Sub
+
+    Public Sub BtnRightParenthesis_Click()
+        AppendChar(")"c)
+    End Sub
+
+    Public Sub BtnPow_Click()
+        AppendChar("^"c)
+    End Sub
+
+    Public Sub BtnBackspace_Click()
+        If TblValue.text.Length > 1 Then
+            TblValue.text = TblValue.text.Substring(0, TblValue.text.Length - 1)
+        Else
+            TblValue.text = "0"
+        End If
+        justEvaled = False
+        justFaulted = False
+    End Sub
+
+    Public Sub BtnClear_Click()
+        TblValue.text = "0"
+        justEvaled = False
+        justFaulted = False
+    End Sub
+End Class
